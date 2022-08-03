5d87d4522ecb4d8ebdffeb18ee8d759b80a0f1ae
gamer99122 <gamer99122@hotmail.com.tw>
2022-08-03 10:58:54 +0800

    First Commit

---
+++ Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Web;
+
+namespace WebGetClientIP
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string _IP = string.Empty;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                _IP = GetClientIPv4();
+                labClientIP.Text = _IP;
+            }
+        }
+
+        public static string GetClientIPv4()
+        {
+            string ipv4 = String.Empty;
+            foreach (IPAddress ip in Dns.GetHostAddresses(GetClientIP()))
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    ipv4 = ip.ToString();
+                    break;
+                }
+            }
+            if (ipv4 != String.Empty) { return ipv4; }
+            foreach (IPAddress ip in Dns.GetHostEntry(GetClientIP()).AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    ipv4 = ip.ToString();
+                    break;
+                }
+            }
+            return ipv4;
+        }
+
+        public static string GetClientIP()
+        {
+            if (null == HttpContext.Current.Request.ServerVariables["HTTP_VIA"])
+            {
+                return HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+            }
+            else
+            {
+                return HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            }
+        }
+    }
+}
